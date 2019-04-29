ffe5c2c68ded1af321220066ab7fa607cca3fa3b
Fabricio Pires Obregon <fabricio@serti.com.br>
2019-04-29 10:27:28 -0600

    Fixing saving book to backend

--- src/bookcomponents/FetchGBooks.js
+++ src/bookcomponents/FetchGBooks.js
@@ -1,55 +1,55 @@
 import React, { useState, useEffect } from "react";
-import 'purecss/build/pure.css';
-import '../css/side-menu.css';
-
 
 export default function FetchGBooks(props){
     const [isbn, setIsbn] = useState("");
     const [fetchJSON, setFetchJSON] = useState(undefined);
+    const [responseJSON, setResponseJSON] = useState(undefined);
+    const [customIsbn, setCustomIsbn] = useState("");
     const [imageUrl, setImageUrl] = useState("");
     const [title, setTitle] = useState("");
-    const [categorie, setCategorie] = useState("");
+    const [category, setCategory] = useState("");
     const [year, setYear] = useState("");
     const [author, setAuthor] = useState("");
     const [publisher, setPublisher] = useState("");
     const [pageCount, setPageCount] = useState("");
     const [description, setDescription] = useState("");
 
-    useEffect(() => {
-        // Update the document title using the browser API
-        if(isbn === "" || isbn.length < 13){
-            setFetchJSON("");
-            setImageUrl("");
-            setTitle("");
-            setCategorie("");
-            setYear("");
-            setAuthor("");
-            setPublisher("");
-            setPageCount("");
-            setDescription("");
-        }else{
-            if(fetchJSON){
-                if(fetchJSON.totalItems > 0){
-                    setImageUrl(JSON.stringify(fetchJSON.items[0].volumeInfo.imageLinks.thumbnail));
-                    setTitle(JSON.stringify(fetchJSON.items[0].volumeInfo.title));
-                    setCategorie(JSON.stringify(fetchJSON.items[0].volumeInfo.categories[0]));
-                    setYear(JSON.stringify(fetchJSON.items[0].volumeInfo.publishedDate.substr(0, 4)));
-                    setAuthor(JSON.stringify(fetchJSON.items[0].volumeInfo.authors[0]));
-                    setPublisher(JSON.stringify(fetchJSON.items[0].volumeInfo.publisher));
-                    setPageCount(JSON.stringify(fetchJSON.items[0].volumeInfo.pageCount));
-                    setDescription(JSON.stringify(fetchJSON.items[0].volumeInfo.description));
-                }
+
+    // useEffect(() => {
+    //     if (fetchJSON === null){
+    //         populateForm();
+    //     }
+    // });
+
+    function callPopulateForm() {populateForm();}
+
+    function populateForm() {
+            if(isbn.length === 13 && fetchJSON.totalItems > 0){
+                setImageUrl(JSON.stringify(fetchJSON.items[0].volumeInfo.imageLinks.thumbnail).replace(/['"]+/g, ''));
+                setTitle(JSON.stringify(fetchJSON.items[0].volumeInfo.title).replace(/['"]+/g, ''));
+                setCategory(JSON.stringify(fetchJSON.items[0].volumeInfo.categories[0]).replace(/['"]+/g, ''));
+                setYear(JSON.stringify(fetchJSON.items[0].volumeInfo.publishedDate.substr(0, 4)).replace(/['"]+/g, ''));
+                setAuthor(JSON.stringify(fetchJSON.items[0].volumeInfo.authors[0]).replace(/['"]+/g, ''));
+                setPublisher(JSON.stringify(fetchJSON.items[0].volumeInfo.publisher).replace(/['"]+/g, ''));
+                setPageCount(JSON.stringify(fetchJSON.items[0].volumeInfo.pageCount).replace(/['"]+/g, ''));
+                setDescription(JSON.stringify(fetchJSON.items[0].volumeInfo.description).replace(/['"]+/g, ''));
+                setCustomIsbn(JSON.stringify(fetchJSON.items[0].volumeInfo.industryIdentifiers[0].identifier).replace(/['"]+/g, ''));
+            }else{
+                setFetchJSON("");
+                setImageUrl("");
+                setTitle("");
+                setCategory("");
+                setYear("");
+                setAuthor("");
+                setPublisher("");
+                setPageCount("");
+                setDescription("");
+                setCustomIsbn("");
             }
+
         }
-    });
 
-    useEffect(() => {
-        // Update the document title using the browser API
-
-
-    });
-
-    function onSubmit(props) {
+    function requestGBooks(props) {
         props.preventDefault();
 
         if(isbn === "" || isbn.length < 13){
@@ -65,27 +65,75 @@ export default function FetchGBooks(props){
                         throw new Error('ISBN not found');
                     }
                 })
-                .then(json => {setFetchJSON(json);})
+                .then(json => {setFetchJSON(json);console.log("Here is your fetch " + responseJSON);callPopulateForm();})
+                .catch(error => {alert(error.message)});
+        }
+    }
+
+    function saveBook(props) {
+        props.preventDefault();
+        if(isbn.length === 13){
+
+            const token = JSON.parse(localStorage.getItem('Authentication'));
+            const userId = token == null ? "":token.id;
+
+            console.log(
+                JSON.stringify({
+                    isbn: customIsbn,
+                    title: title,
+                    description : description,
+                    imageUrl: imageUrl,
+                    publisher: publisher,
+                    year: year,
+                    category: category,
+                    pageCount: pageCount,
+                    authors: ([{name:author}]),
+                    customer_id: userId
+                })
+            )
+
+            fetch("http://localhost:9000/book/add", {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    isbn: customIsbn,
+                    title: title,
+                    description : description,
+                    imageUrl: imageUrl,
+                    publisher: publisher,
+                    year: year,
+                    category: category,
+                    pageCount: pageCount,
+                    authors: ([{name:author}]),
+                    customer_id: userId
+                })
+            })
+                .then(response =>  {
+                    if(response.ok) {
+                        alert("Your book was saved!");
+                        return response.json();
+                    } else {
+                        throw new Error('Error saving');
+                    }
+                })
+                .then(json => {setResponseJSON(json);})
                 .catch(error => {alert(error.message)});
         }
     }
 
     const changeIsbn = event => {setIsbn(event.target.value);}
+    const changeCustomIsbn = event => {setCustomIsbn(event.target.value);}
     const changeImageUrl = event => {setImageUrl(event.target.value);}
+    const changeYear = event => {setYear(event.target.value);}
     const changeTitle = event => {setTitle(event.target.value);}
-    const changeCategorie = event => {setCategorie(event.target.value);}
+    const changeCategory = event => {setCategory(event.target.value);}
     const changeAuthor = event => {setAuthor(event.target.value);}
     const changePublisher = event => {setPublisher(event.target.value);}
     const changePagecount = event => {setPageCount(event.target.value);}
     const changeDescription = event => {setDescription(event.target.value);}
 
-
-
-
-
-
-
     return (
+
 <div>
         <form className="pure-form pure-form-stacked">
             <fieldset>
@@ -93,13 +141,13 @@ export default function FetchGBooks(props){
                     <div className="pure-u-1 pure-u-md-1-3">
                         <label htmlFor="isbn">GoogleAPI</label>
                         <input type="text" id="isbn" className="pure-input-rounded" onChange={changeIsbn}/>
-                        <button type="submit" className="pure-button pure-button-primary" onClick={onSubmit}>Search by ISBN</button>
+                        <button type="submit" className="pure-button pure-button-primary" onClick={requestGBooks}>Search by ISBN</button>
                     </div>
 
-                    {/*<div className="pure-u-1 pure-u-md-1-3">*/}
-                    {/*    <label htmlFor="imageUrl">Image Url</label>*/}
-                    {/*    <input id="imageUrl" className="pure-u-23-24" type="text" value={imageUrl} onChange={changeTitle}/>*/}
-                    {/*</div>*/}
+                    <div className="pure-u-1 pure-u-md-1-3">
+                        <label htmlFor="customIsbn">Isbn</label>
+                        <input id="customIsbn" className="pure-u-23-24" type="text" value={customIsbn} onChange={changeCustomIsbn}/>
+                    </div>
 
                     <div className="pure-u-1 pure-u-md-1-3">
                         <label htmlFor="title">Title</label>
@@ -107,18 +155,23 @@ export default function FetchGBooks(props){
                     </div>
 
                     <div className="pure-u-1 pure-u-md-1-3">
-                        <label htmlFor="categorie">Categorie</label>
-                        <input id="categorie" className="pure-u-23-24" type="text" value={categorie} onChange={changeCategorie}/>
+                        <label htmlFor="category">Category</label>
+                        <input id="category" className="pure-u-23-24" type="text" value={category} onChange={changeCategory}/>
                     </div>
 
                     <div className="pure-u-1 pure-u-md-1-3">
                         <label htmlFor="author">Author</label>
-                        <input id="author" className="pure-u-23-24" type="email" required value={author} onChange={changeAuthor}/>
+                        <input id="author" className="pure-u-23-24" type="  email" required value={author} onChange={changeAuthor}/>
                     </div>
 
                     <div className="pure-u-1 pure-u-md-1-3">
                         <label htmlFor="publisher">Publisher</label>
                         <input id="publisher" className="pure-u-23-24" type="text" value={publisher} onChange={changePublisher}/>
+                    </div>
+
+                    <div className="pure-u-1 pure-u-md-1-3">
+                        <label htmlFor="year">Year</label>
+                        <input id="year" className="pure-u-23-24" type="text" value={year} onChange={changeYear}/>
                     </div>
 
                     <div className="pure-u-1 pure-u-md-1-3">
@@ -128,19 +181,22 @@ export default function FetchGBooks(props){
 
                     <div className="pure-u-1 pure-u-md-1-3">
                         <label htmlFor="description">Description</label>
-                        <input id="description" className="pure-u-23-24" type="text" value={description} onChange={changeIsbn}/>
+                        <input id="description" className="pure-u-23-24" type="text" value={description} onChange={changeDescription}/>
+                    </div>
+
+                    <div className="pure-u-1 pure-u-md-1-3">
+                        <label htmlFor="imageUrl">Image Url</label>
+                        <input id="imageUrl" className="pure-u-23-24" type="text" value={imageUrl} onChange={changeImageUrl}/>
                     </div>
 
                 </div>
 
-                <button type="submit" className="pure-button pure-button-primary">Submit</button>
+                <button type="submit" className="pure-button pure-button-primary" onClick={saveBook}>Save</button>
             </fieldset>
         </form>
 
 
 </div>
-
-
 
     );
 
